95184a6bd7197923159e05bedab9640e1e543fa0
Callen Schultz <callen.schultz@gmail.com>
2021-10-21 17:29:38 -0500

    added css

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ app.get('/', function (req,res) {
     res.sendFile(path.join(__dirname, "../client/client.html"))
 })
 
-app.use('/css', express.static(path.join(__dirname, 'public/client.css')))
+app.get('/css', function (req,res) {
+    res.sendFile(path.join(__dirname, "../client.css"))
+})
 
 const port = process.env.PORT || 4321
 
