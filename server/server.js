722bb23b27a521a269954ad2349c92294444677b
Callen Schultz <callen.schultz@gmail.com>
2021-10-21 15:17:59 -0500

    Adding some CSS and a Dropdown Menu

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ app.get('/', function (req,res) {
     res.sendFile(path.join(__dirname, "../client/client.html"))
 })
 
+app.use('/css', express.static(path.join(__dirname, 'client/client.css')))
+
 const port = process.env.PORT || 4321
 
 app.listen(port, () => console.log(`Rockin' and Rollin' on ${port}`))
