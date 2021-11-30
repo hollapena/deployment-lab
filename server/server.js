ef782d7e3a662a1ec5931d05a532f3b6e6c45d3d
Callen Schultz <callen.schultz@gmail.com>
2021-11-30 16:50:51 -0600

    button styling

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.get('/css', function (req,res) {
     res.sendFile(path.join(__dirname, "../client/client.css"))
 })
 
+
 const port = process.env.PORT || 4321
 
 app.listen(port, () => console.log(`Rockin' and Rollin' on ${port}`))
